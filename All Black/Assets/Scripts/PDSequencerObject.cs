926446579bf1cf60505cc4ed058ef8f5ac64a60d
Anthony Licata <tamamaroro@gmail.com>
2016-11-01 23:07:39 -0400

    updated instruments I think?

---
+++ All Black/Assets/Scripts/PDSequencerObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PDSequencerObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PureData.PlaySequence("test");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
