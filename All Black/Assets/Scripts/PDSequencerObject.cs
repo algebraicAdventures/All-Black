c4756ec48a0076447df94817467494b8e33de38d
Anthony Licata <tamamaroro@gmail.com>
2016-11-05 12:14:21 -0400

    Working spatialization

--- All Black/Assets/Scripts/PDSequencerObject.cs
+++ All Black/Assets/Scripts/PDSequencerObject.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class PDSequencerObject : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        PureData.PlaySequence("test");
+    PureDataSequenceItem sequenceItem;
+    Vector2 scroll;
+
+    void OnGUI()
+    {
+        GUILayout.Label("Current Item: " + (sequenceItem == null ? "None" : sequenceItem.ToString()));
+
+        GUILayout.Space(16);
+
+        scroll = GUILayout.BeginScrollView(scroll, GUILayout.Width(Screen.width - 50));
+
+        GUILayout.Label("Plays the sequence named 'test'. You can watch it's progress in the PureData script under Sequences.");
+        if (GUILayout.Button("Play"))
+        {
+            sequenceItem = PureData.PlaySequence("test");
+        }
+
+        GUILayout.Space(8);
+
+        if (sequenceItem != null)
+        {
+            GUILayout.Label("Stops the sequence if it is still playing.");
+            if (GUILayout.Button("Stop"))
+            {
+                sequenceItem.Stop();
+                sequenceItem = null;
+            }
+        }
+
+        GUILayout.EndScrollView();
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
