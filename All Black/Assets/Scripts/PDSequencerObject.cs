8bcede13420c3a29913fd6443b40dc869dc70b85
Anthony Licata <tamamaroro@gmail.com>
2016-11-10 19:15:41 -0500

    Final Build

--- All Black/Assets/Scripts/PDSequencerObject.cs
+++ All Black/Assets/Scripts/PDSequencerObject.cs
@@ -3,35 +3,13 @@ using System.Collections;
 
 public class PDSequencerObject : MonoBehaviour {
 
-    PureDataSequenceItem sequenceItem;
+    PureDataSequenceItem sequenceItem1, sequenceItem2, sequenceItem3;
     Vector2 scroll;
 
-    void OnGUI()
+    void Start()
     {
-        GUILayout.Label("Current Item: " + (sequenceItem == null ? "None" : sequenceItem.ToString()));
-
-        GUILayout.Space(16);
-
-        scroll = GUILayout.BeginScrollView(scroll, GUILayout.Width(Screen.width - 50));
-
-        GUILayout.Label("Plays the sequence named 'test'. You can watch it's progress in the PureData script under Sequences.");
-        if (GUILayout.Button("Play"))
-        {
-            sequenceItem = PureData.PlaySequence("test");
-        }
-
-        GUILayout.Space(8);
-
-        if (sequenceItem != null)
-        {
-            GUILayout.Label("Stops the sequence if it is still playing.");
-            if (GUILayout.Button("Stop"))
-            {
-                sequenceItem.Stop();
-                sequenceItem = null;
-            }
-        }
-
-        GUILayout.EndScrollView();
+        sequenceItem1 = PureData.PlaySequence("1");
+        sequenceItem2 = PureData.PlaySequence("2");
+        sequenceItem3 = PureData.PlaySequence("3");
     }
 }
